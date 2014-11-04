50825776e82c32469c84f7c984252ee1d850d332
rgarcia <voodoorai2000@gmail.com>
2014-11-04 17:18:55 +0100

    Adding selected menu link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,19 @@
 module ApplicationHelper
 
+  def menu_link(title, path, options={})
+    content_tag :li do
+      link_to title, path, options.merge(selected(title))
+    end
+  end
+
+  def selected(title)    
+    (request.original_url.match(/#{title}/i) or home(title)) ? {class: 'selected'} : {}
+  end
+
+  def home(title)
+    title == 'Artwork' and request.original_url == root_url
+  end
+
   def name(category)
     category.gsub("_", " ")
   end
