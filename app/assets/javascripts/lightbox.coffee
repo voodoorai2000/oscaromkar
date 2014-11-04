3f778c965e49141a162d987c175059a3fb05b027
rgarcia <voodoorai2000@gmail.com>
2014-11-04 16:57:46 +0100

    DRYing up artwork and project categories

--- app/assets/javascripts/lightbox.coffee
+++ app/assets/javascripts/lightbox.coffee
@@ -1,7 +1,5 @@
 ready = ->
-  artwork    = ["BlackAndWhite", "Creativity", "Digital", "Frotage", "Ink", "Inspirations", "Paint", "Pencil", "WaterColor"]
-  projects   = ["Anne_Frank_Workshop", "Avatar_Mural", "Bubbles", "Caravan_Cabin", "Didjeridoo_Workshop", "Horse_Box", "Magic_Bus", "New_York_Skyline", "Truck_Mural", "WC_Shed"]
-  categories = artwork.concat projects
+  categories = gon.categories if gon
 
   for category in categories
     $(".#{category}").colorbox({rel:"#{category}", maxHeight: '100%'});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,30 @@
 class PagesController < ApplicationController
+  before_action :set_javascript_variables, except: [:about]
 
   def artwork
-    @categories = ["BlackAndWhite", "Creativity", "Digital", "Frotage", "Ink", "Inspirations", "Paint", "Pencil", "WaterColor"]
+    @categories = artwork_categories
   end
 
   def projects
-    @categories = ["Anne_Frank_Workshop", "Avatar_Mural", "Bubbles", "Caravan_Cabin", "Didjeridoo_Workshop", "Horse_Box", "Magic_Bus", "New_York_Skyline", "Truck_Mural", "WC_Shed"]
+    @categories = project_categories
   end
 
   def about
     @categories = []
+  end
+
+  private
+
+  def artwork_categories
+    ["BlackAndWhite", "Creativity", "Digital", "Frotage", "Ink", "Inspirations", "Paint", "Pencil", "WaterColor"]
+  end
+
+  def project_categories
+    ["Anne_Frank_Workshop", "Avatar_Mural", "Bubbles", "Caravan_Cabin", "Didjeridoo_Workshop", "Horse_Box", "Magic_Bus", "New_York_Skyline", "Truck_Mural", "WC_Shed"]
+  end
+
+  def set_javascript_variables
+    gon.categories = artwork_categories + project_categories
   end
 
 end
