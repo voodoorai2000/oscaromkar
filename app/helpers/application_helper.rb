6e42801cda643d5f1d82320e662481b68cb93050
rgarcia <voodoorai2000@gmail.com>
2014-11-04 15:20:41 +0100

    Displaying correct image count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
   def image_name(category, index)
     "/assets/Artwork/#{category}/#{index}.jpg"
   end
+
+  def folder(category)
+    "#{Rails.root}/app/assets/images/Artwork/#{category}/"
+  end
+
+  def image_count(category)
+    dir = folder(category)
+    Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) } - 1
+  end
 end
